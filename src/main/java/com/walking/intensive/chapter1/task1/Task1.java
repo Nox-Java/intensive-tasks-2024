b972bad381cf3ac9afcfb3cd5945573bd4a14a97
Nox <soreo45@yahoo.com>
2024-11-18 16:42:52 +0300

    Готово

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,30 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = -5;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if (age >= 0 && age <= 127) {
 
-        return null; // Заглушка. При реализации - удалить
+            if (age % 10 == 1 && age % 100 != 11) {
+                return "Вам " + age + " год.";
+            }
+            if (age % 10 == 2 && age % 100 != 12) {
+                return "Вам " + age + " года.";
+            }
+            if (age % 10 == 3 && age % 100 != 13) {
+                return "Вам " + age + " года.";
+            }
+            if (age % 10 == 4 && age % 100 != 14) {
+                return "Вам " + age + " года.";
+            }
+            return "Вам " + age + " лет.";
+        }
+        return "Либо Вы еще не родились, либо уже того...";
+
     }
 }
+
